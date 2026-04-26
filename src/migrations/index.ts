2f6da69d4e4cdda217dd9557be45994fd31c2b3a
mirco123 <miroslav.liska@gmail.com>
2026-04-26 11:55:00 +0200

    database update

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260408_092611 from './20260408_092611';
+import * as migration_20260426_094500 from './20260426_094500';
 
 export const migrations = [
   {
     up: migration_20260408_092611.up,
     down: migration_20260408_092611.down,
     name: '20260408_092611'
+  },
+  {
+    up: migration_20260426_094500.up,
+    down: migration_20260426_094500.down,
+    name: '20260426_094500'
   },
 ];
